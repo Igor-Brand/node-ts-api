748cfe10d793dcc1cf0b69d5832c086ddf9996c0
Igor Brandão <111445872+Igor-Brand@users.noreply.github.com>
2026-04-30 20:14:29 -0300

    feat: desenvolvimento do controller inicial

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 import { StatusCodes } from 'http-status-codes';
 import 'dotenv/config';
 
+import { CidadesController } from "../controllers";
 
 const router = Router();
 
 router.get('/', (req, res) => {
     return res.status(StatusCodes.OK).send('Hello, World!');
 });
+
+router.post('/cidades', CidadesController.create);
 
 export { router };
